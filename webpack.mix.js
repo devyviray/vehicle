1cba9ca47afe5f3b20e7c4f049349dc4bf72a2e1
devy.viray <demetrio.viray@lafilgroup.com>
2019-03-05 19:19:59 +0800

    initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+// mix.js('resources/js/app.js', 'public/js')
+//    .sass('resources/sass/app.scss', 'public/css');
+
+
+mix.styles([
+    'public/css/app.css'
+	// 'public/css/argon.css'
+	
+], 'public/css/all.css')
+.js([
+	'resources/js/app.js',
+    // 'node_modules/popper.js/dist/popper.js',
+    // 'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    // 'public/vendor/chart.js/dist/Chart.min.js',
+    // 'public/vendor/chart.js/dist/Chart.extension.js',
+    // 'public/js/argon.js?'
+], 'public/js/all.js')
+.browserSync('http://vehicle.local');
