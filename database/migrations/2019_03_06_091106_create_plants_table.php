b058e9b2be9d92e92816fb8608875d63219b4b0f
devy.viray <demetrio.viray@lafilgroup.com>
2019-03-07 20:30:14 +0800

    table migrations

---
+++ database/migrations/2019_03_06_091106_create_plants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('plant_code', 255);
+            $table->string('plant_name', 255);
+            $table->string('company_server', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plants');
+    }
+}
---
+++ database/migrations/2019_03_06_101309_create_vendors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVendorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('vendor_code', 255);
+            $table->string('name', 255);
+            $table->string('name2', 255);
+            $table->string('company_server', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+}
