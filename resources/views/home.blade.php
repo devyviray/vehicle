974249abcb070e14d4464017bcf0c21d132d2445
Devy Viray <demetrio.viray@lfuggoc.net>
2023-02-19 20:28:45 +0800

    date format to fixed data missing error

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <dashboard :user-level="{{ Auth::user()->level() }}" role="{{ Auth::user()->roles[0]->name }}" ></dashboard>
+    {{-- <dashboard :user-level="{{ Auth::user()->level() }}" role="{{ Auth::user()->roles[0]->name }}" ></dashboard> --}}
+    <h1>SAMPLE</h1>
 @endsection
 
 @push('js')
