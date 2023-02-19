d6af9bbfb0b98cd1c0a7552e818a212ec3c8c8a3
Devy Viray <demetrio.viray@lfuggoc.net>
2023-02-20 03:13:52 +0800

    Fixed data missing error

--- app/GpsDevice.php
+++ app/GpsDevice.php
@@ -8,6 +8,12 @@ use OwenIt\Auditing\Contracts\Auditable;
 class GpsDevice extends Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    public function getDates()
+    {
+        return [];
+    }
     
     protected $fillable = [
         'vehicle_id',
--- app/GpsDeviceAttachment.php
+++ app/GpsDeviceAttachment.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class GpsDeviceAttachment extends Model
 {
-    //
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    public function getDates()
+    {
+        return [];
+    }
 }
