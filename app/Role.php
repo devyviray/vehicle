211a74e4adea1ea6def3a979019016ce2e75eb2f
chrollo <jay.lumagdong@gmail.com>
2023-02-20 02:39:49 +0800

    patch update

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    public function getDates()
+    {
+        return [];
+    }
+
+    protected $hidden = [
+        'created_at','updated_at'
+    ];
+    
 }
