ad2c717ce26d79e6deb95aad8af949d773a5cecb
Devy Viray <demetrio.viray@lfuggoc.net>
2023-02-20 03:18:01 +0800

    Fixed data missing error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,6 @@ class UserController extends Controller
      */
     public function index()
     {
-        return User::all();
         session(['header_text' => 'Users']);
 
         return view('user.index');
