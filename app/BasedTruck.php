33d7a9fb7844ef74f2dea2fcc3707b2b69cc4296
chrollo <jay.lumagdong@gmail.com>
2023-02-20 02:37:31 +0800

    patch update

--- app/BasedTruck.php
+++ app/BasedTruck.php
@@ -7,13 +7,15 @@ use OwenIt\Auditing\Contracts\Auditable;
 
 class BasedTruck extends Model implements Auditable
 {
+    protected $dateFormat = 'Y-m-d H:i:s';
+    
     use \OwenIt\Auditing\Auditable;
 
     public function getDates()
     {
         return [];
     }
-    
+
     protected $hidden = [
         'created_at','updated_at'
     ];
