0a97d5286de1b7b893cf04e5da23b5326269126e
Devy Viray <demetrio.viray@lfuggoc.net>
2023-02-19 21:45:07 +0800

    date format to fixed data missing error

--- app/User.php
+++ app/User.php
@@ -50,11 +50,24 @@ class User extends Authenticatable implements Auditable
     // protected $casts = [
     //     'email_verified_at' => 'date:Y-m-d',
     // ];
-     protected $casts = [
-        'email_verified_at' => 'datetime:Y-m-d H:i:s',
-        'created_at' => 'datetime:Y-m-d H:i:s',
-        'updated_at' => 'datetime:Y-m-d H:i:s',
-    ];
+
+    public function getDateFormat()
+    {
+        return 'Y-m-d H:i:s.u';
+    }
+
+    /**
+     * Convert a DateTime to a storable string.
+     * SQL Server will not accept 6 digit second fragment (PHP default: see getDateFormat Y-m-d H:i:s.u)
+     * trim three digits off the value returned from the parent.
+     *
+     * @param  \DateTime|int  $value
+     * @return string
+     */
+    public function fromDateTime($value)
+    {
+        return substr(parent::fromDateTime($value), 0, -3);
+    }
 
     public function basedTrucks(){
         return $this->belongsToMany(BasedTruck::class);
