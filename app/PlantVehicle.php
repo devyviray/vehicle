a542d67f5c2b1bb9ce2a1a59697a4f4629e26684
Devy Viray <demetrio.viray@lfuggoc.net>
2023-03-30 20:14:11 +0800

    Apply soft delete to plant vehicle model

--- app/PlantVehicle.php
+++ app/PlantVehicle.php
@@ -4,10 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PlantVehicle extends Model implements Auditable
 {
-    use \OwenIt\Auditing\Auditable;
+    use \OwenIt\Auditing\Auditable,SoftDeletes;
 
     protected $table = 'plant_vehicle';
 }
