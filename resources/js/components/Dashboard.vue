c9d8a85c7b6ddca25d55a1ea62b9540056f154d7
Devy Viray <demetrio.viray@lfuggoc.net>
2023-03-31 18:19:03 +0800

    Fixed download to excel vendor description issue

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -943,7 +943,7 @@ export default {
                     "CATEGORY": data.category.description,
                     "PLATE NUMBER": data.plate_number,
                     "PLANT INDICATOR": data.indicator.description,
-                    "VENDOR": data.vendor ? data.vendor.vendor_description_lfug : data.vendor,
+                    "VENDOR": data.vendor.vendor_description_lfug,
                     "SUBCON VENDOR": data.subcon_vendor ? data.subcon_vendor.vendor_description_lfug : '',
                     "CAPACITY": data.capacity.description,
                     "GOODS": data.good ? data.good.description : '',
