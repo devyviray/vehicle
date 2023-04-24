eeb44a35a752a3455f11f0ba92d83d30ef9b99c5
Devy Viray <demetrio.viray@lfuggoc.net>
2023-04-24 09:06:17 +0800

    Validity rule fixes

--- app/Rules/ValidityRule.php
+++ app/Rules/ValidityRule.php
@@ -38,6 +38,7 @@ class ValidityRule implements Rule
         
         if($vehicles && $this->validityStartDate){
             foreach($vehicles as $vehicle){ 
+                dd('end date '. $vehicle->validity_end_date. 'start date '.$this->validityStartDate);
                 if($vehicle->validity_end_date >=  $this->validityStartDate){
                     $error = $error + 1;
                 }
