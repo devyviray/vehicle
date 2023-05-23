95fb03619b9a0048b011cc04b6ed99c62b68322b
chrollo <jay.lumagdong@gmail.com>
2023-05-23 08:03:10 +0800

    patch update : remove gps device

--- app/GpsDevice.php
+++ app/GpsDevice.php
@@ -4,11 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GpsDevice extends Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
     protected $dateFormat = 'Y-m-d H:i:s';
+
+    use SoftDeletes;
 
     public function getDates()
     {
--- app/Http/Controllers/GpsDevicesController.php
+++ app/Http/Controllers/GpsDevicesController.php
@@ -224,12 +224,9 @@ class GpsDevicesController extends Controller
             $destroy_api = $this->destroy_api_gps($gps_device->device_id);
             $destroy_gps_attachments = GpsDeviceAttachment::where('vehicle_id', $gps_device->vehicle_id)->delete();
             if($destroy_api == "Success"){
-                
-                if($gps_device->delete()){
-                    // DB::commit();
-                    $vehicle = Vehicle::with('category','capacity', 'indicator', 'good', 'basedTruck', 'contract', 'documents', 'user', 'vendor', 'subconVendor', 'plants','gpsdevice','gpsdeviceattachments')->where('id', $gps_device->vehicle_id)->first();
-                    return $vehicle;
-                }
+                GpsDevice::where('id',$gps_device->id)->delete();
+                $vehicle = Vehicle::with('category','capacity', 'indicator', 'good', 'basedTruck', 'contract', 'documents', 'user', 'vendor', 'subconVendor', 'plants','gpsdevice','gpsdeviceattachments')->where('id', $gps_device->vehicle_id)->first();
+                return $vehicle;
             }else{
                 // DB::rollBack();
                 $vehicle = Vehicle::with('category','capacity', 'indicator', 'good', 'basedTruck', 'contract', 'documents', 'user', 'vendor', 'subconVendor', 'plants','gpsdevice','gpsdeviceattachments')->where('id', $gps_device->vehicle_id)->first();
