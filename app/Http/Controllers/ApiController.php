a2285c337ccbc765eb3f8387fce169aee789ff5f
Arjay Lumagdong <jay.lumagdong@gmail.com>
2024-02-27 11:04:24 +0800

    vehicle status

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -21,30 +21,23 @@ class ApiController extends Controller
 
    public function plateNumberStatus(Request $request){
 
+    $expiration_date = Carbon::now();
+
     $plate_number = isset($request->plate_number) ? $request->plate_number : "";
 
     $vehicle = Vehicle::orderBy('validity_start_date', 'desc');
 
     if($plate_number){
-        $vehicle->where('plate_number',$plate_number);
+        $vehicle->where('plate_number',$plate_number)
+                        ->whereDate('validity_end_date','>',$expiration_date);
     }
 
     $vehicle = $vehicle->first();
 
     if($vehicle){
-        $date_today = date('Y-m-d');
-
-        $validity_end_date = Carbon::parse('Y-m-d', $vehicle->validity_end_date)->format('Y-m-d');
-        // $formatted_validity_end_date = $validity_end_date->format('Y-m-d');
-
-        dump($date_today);
-        dump($validity_end_date);
-        
-        if($validity_end_date < $date_today) {
-            return 'Expired';
-        }else{
-            return "Valid";
-        }
+        return 'Valid';
+    }else{
+        return 'Expired';
     }
    }
 }
